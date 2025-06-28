723d88eee8b6de49d64f466ca20f034a56ed247e
Abdul Rauf <editsabdulrauf5681@gmail.com>
2025-06-28 11:16:15 +0000

    updated design to minimalism

--- media.js
+++
@@ -1,6 +0,0 @@
-var resumeCtas = document.querySelectorAll('.resume-cta');
-
-resumeCtas.forEach(resumeCta => {
-    resumeCta.setAttribute('href', 'https://drive.google.com/file/d/1S6Yf2S21PcCrBYkouA_IK4tF7YSVbw3I/view');
-});
-
--- script.js
+++ script.js
@@ -1,67 +1,10 @@
-let themeCta = document.querySelector(".theme-cta");
-
-themeCta.addEventListener("click", () => {
+let isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+if(isDark){
   document.documentElement.classList.toggle("second");
-  themeCta.classList.toggle("dark");
-  setTimeout(() => {
-    changeDepart();
-  }, 50);
-});
-
-window.addEventListener('scroll' , reveal);
-
-var connectIcons = document.querySelectorAll('.connect-icon');
-var connectTexts = document.querySelector('.connect-txt');
-
-function reveal() {
-  
-  for (var i = 0; i < connectIcons.length; i++){
-    var windowHeight = window.innerHeight;
-    var revealTop = connectIcons[i].getBoundingClientRect().top;
-    var revealPoint = -50; 
-
-    if(revealTop < windowHeight - revealPoint){
-      connectIcons[i].classList.add('show');
-    } else {
-      connectIcons[i].classList.remove('show');
-    }
-    if(connectIcons[i].classList.contains('show')){
-      connectTexts.classList.add('show');
-    } else {
-      connectTexts.classList.remove('show');
-    }
-  }
-
 }
+
+
 
 // window.addEventListener('scroll', () => {
 //   document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
-// }, false);
-
-let menuBtn = document.querySelector('.menu-icon');
-let navList = document.querySelectorAll('.nav__list-item');
-let navContent = document.querySelector('.nav-wrapper');
-let navListItem = document.querySelectorAll('.nav__list-item a');
-
-function menuActive(){
-  menuBtn.addEventListener('click' ,function () {
-    menuBtn.classList.toggle('menu_active');
-    navContent.classList.toggle('menu_active');
-    navList.forEach((navList) => navList.classList.toggle('menu_active'));
-    themeCta.classList.toggle('menu_active');
-  });
-};
-
-menuActive();
-
-function menuInactive(){
-  navListItem.forEach((navListItem) => navListItem.addEventListener('click' , function() {
-    menuBtn.classList.remove('menu_active');
-    navContent.classList.remove('menu_active');
-    navList.forEach((navList) => navList.classList.remove('menu_active'));
-    themeCta.classList.remove('menu_active');
-  }));
-};
-
-menuInactive();
-
+// }, false)
